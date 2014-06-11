2ab22e2714d2c4d91f3ef1a2a7adae1ef54977cf
Ivan Fomichev <ifomichev@gmail.com>
2014-06-12 00:30:23 +0200

    better filtering

--- filter-monochromatic.py
+++ filter-monochromatic.py
@@ -24,21 +24,41 @@ infile = open(filename, 'r')
 outfile = open(filename + '.out', 'w')
 
 for line in infile:
-  pattern = re.split('\s', line, 2)[0]
+  pattern, _, _, pattern_type = re.split('\s', line, 5)[:4]
+  if pattern_type != 'other':
+    continue
+
   pattern = re.sub('!', '.\n', pattern)
 
   cells = g.parse(pattern)
 
   gliders = []
+  temp_cells = cells[:]
   for gen in range(0, 4):
-    gliders.extend(find_all_gliders(cells))
-    cells = g.evolve(cells, 1)
+    gliders.extend(find_all_gliders(temp_cells))
+    temp_cells = g.evolve(temp_cells, 1)
 
   if len(gliders) < 2:
     continue
 
   sample = get_glider_color(gliders[0])
-  if all([get_glider_color(glider) == sample for glider in gliders[1:]]):
+  if not all([get_glider_color(glider) == sample for glider in gliders[1:]]):
+    continue
+
+  # check that pattern emits exactly one orthogonal glider
+  new_cells = g.evolve(cells, 700)
+  emitted_gliders = []
+  for t in range(0, 4):
+    emitted_gliders.append(0)
+
+    temp_cells = new_cells
+    for gen in range(0, 4):
+      emitted_gliders[-1] += len(find_all_gliders(temp_cells))
+      temp_cells = g.evolve(temp_cells, 1)
+
+    new_cells = g.transform(new_cells, 0, 0, 0, -1, 1, 0)
+
+  if emitted_gliders[0] + emitted_gliders[2] == 0 and emitted_gliders[1] + emitted_gliders[3] == 1:
     outfile.write(line)
 
 infile.close()
